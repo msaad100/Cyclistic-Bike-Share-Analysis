01a1b108ebda7769efb24401979031e13669024d
msaad100 <msrajpoot2006@gmail.com>
2026-03-18 17:34:21 +0500

    Merge monthly trip data into Combined_Trip_Data table
    This script creates a new table 'Combined_Trip_Data' by merging trip data from each month of the year. It also includes a verification step to count the total rows in the new table.

--- SQL_Scripts/01_Data_Merging.sql
+++ SQL_Scripts/01_Data_Merging.sql
@@ -1,1 +1,30 @@
+-- Create table for full year analysis
+SELECT * INTO Combined_Trip_Data
+FROM (
+    SELECT * FROM dbo.Trip_Data_Apr
+    UNION ALL
+    SELECT * FROM dbo.Trip_Data_May
+    UNION ALL
+    SELECT * FROM dbo.Trip_Data_June
+    UNION ALL
+    SELECT * FROM dbo.Trip_Data_July
+    UNION ALL
+    SELECT * FROM dbo.Trip_Data_Aug
+    UNION ALL
+    SELECT * FROM dbo.Trip_Data_Sep
+    UNION ALL
+    SELECT * FROM dbo.Trip_Data_Oct
+    UNION ALL
+    SELECT * FROM dbo.Trip_Data_Nov
+    UNION ALL
+    SELECT * FROM dbo.Trip_Data_Dec
+    UNION ALL
+    SELECT * FROM dbo.Trip_Data_Jan
+    UNION ALL
+    SELECT * FROM dbo.Trip_Data_Feb
+    UNION ALL
+    SELECT * FROM dbo.Trip_Data_Mar
+) AS full_year;
 
+-- Verify total row count
+SELECT COUNT(*) FROM Combined_Trip_Data
